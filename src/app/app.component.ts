eca7660465ad90c6ca3b76a94c91df3008b322bc
Brian Kang <bhkangw@gmail.com>
2018-01-17 16:05:43 -0800

    My changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  time = new Date();
+  lastTimeZoneSelected = null;
+
+  getDateByZone(timezone) {
+    this.time = new Date();
+    if (timezone === 'MST') {
+      this.time.setHours(this.time.getHours() + 1);
+    } else if (timezone === 'CST') {
+      this.time.setHours(this.time.getHours() + 2);
+    } else if (timezone === 'EST') {
+      this.time.setHours(this.time.getHours() + 3);
+    }
+    this.lastTimeZoneSelected = timezone;
+  }
+
 }
